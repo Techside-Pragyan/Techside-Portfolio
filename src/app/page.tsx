de60dc0f30391a657617503ee045980882d0f7bb
Pragyan Paramita Moharana <moharanapragyanparamita@gmail.com>
2026-05-31 13:00:06 +0530

    feat: initialize homepage with hero, about, skills, and tech stack sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import Hero from "@/components/sections/Hero";
 import About from "@/components/sections/About";
+import Skills from "@/components/sections/Skills";
+import TechStack from "@/components/sections/TechStack";
 
 export default function Home() {
   return (
     <main className="w-full h-full flex flex-col">
       <Hero />
       <About />
+      <Skills />
+      <TechStack />
     </main>
   );
 }
