11e9327ac737d5502bc53b30e0ffd6004070cb52
Pragyan Paramita Moharana <moharanapragyanparamita@gmail.com>
2026-05-31 16:59:11 +0530

    feat: implement interactive 3D keyboard component for tech stack visualization

--- src/components/canvas/Keyboard.tsx
+++ src/components/canvas/Keyboard.tsx
@@ -7,7 +7,7 @@ import {
   SiJavascript, SiTypescript, SiHtml5, SiCss, SiReact, SiVuedotjs,
   SiNextdotjs, SiNodedotjs, SiExpress, SiTailwindcss, SiSass, SiWordpress,
   SiPython, SiDjango, SiPostgresql, SiMongodb, SiRedis, SiPrisma,
-  SiGit, SiDocker, SiVercel, SiFigma, SiLinux, SiVite, SiAmazonaws
+  SiGit, SiDocker, SiVercel, SiFigma, SiLinux, SiVite
 } from 'react-icons/si';
 
 const techs = [
