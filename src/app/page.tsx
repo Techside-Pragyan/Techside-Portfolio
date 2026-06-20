72fcb794522d75ceecbea6bd550d85b259c233e4
Pragyan Paramita Moharana <moharanapragyanparamita@gmail.com>
2026-06-20 17:35:10 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import Skills from "@/components/sections/Skills";
 import GithubActivity from "@/components/sections/GithubActivity";
 import Projects from "@/components/sections/Projects";
 import Contact from "@/components/sections/Contact";
+import Timeline from "@/components/sections/Timeline";
 
 export default function Home() {
   return (
